c91a3dde78106b3a79d90f055b728531deb29980
Alexandros <153320564+alekszhs@users.noreply.github.com>
2025-05-05 20:10:52 +0300

    Create quality_checks_silver.sql

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,18 @@
+/*
+================================================================
+Quality Checks
+===================================================================
+Script Purpose:
+  This script performs various quality checks for data consistency, accuracy,
+  and standarization across the silver schemas.It includes checks for:
+  -Null or duplicate primary keys.
+  -Unwanted spaces in string fields.
+  -Data standarization and consistency.
+  -Invalid date ranged and orders.
+  -Data consistency between related fields.
+
+Usage Notes:
+  -Run these checks after data loading Silver layer.
+  -Investigate and resolve any discrepancies found during the checks.
+====================================================================
+*/
